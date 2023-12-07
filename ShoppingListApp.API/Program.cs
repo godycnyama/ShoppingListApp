19efa6ae32804ae8db40a1231bb0a7be7d67bd09
Godknows Christians Nyamatendedza <godycnyama@gmail.com>
2023-12-07 21:28:14 +0200

    added authentication middleware

--- ShoppingListApp.API/Program.cs
+++ ShoppingListApp.API/Program.cs
@@ -1,6 +1,10 @@
 using Carter;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using ShoppingListApp.Application;
 using ShoppingListApp.Infrastructure;
+using ShoppingListApp.Infrastructure.Persistence.Context;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +14,18 @@ builder.Services.AddCarter();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureApplicationServices();
+builder.Services.ConfigureApplicationServices(builder.Configuration);
 builder.Services.ConfigureInfrastructureServices(builder.Configuration);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, c =>
+        {
+            c.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+            c.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidAudience = builder.Configuration["Auth0:Audience"],
+                ValidIssuer = $"{builder.Configuration["Auth0:Domain"]}"
+            };
+        });
 
 
 var app = builder.Build();
@@ -25,8 +39,18 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
 app.MapCarter();
+
+// Migrate latest database changes during startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<ShoppingListAppDataContext>();
+
+    // Here is the migration executed
+    dbContext.Database.Migrate();
+}
 
 app.Run();
--- ShoppingListApp.Application/ServiceExtensions.cs
+++ ShoppingListApp.Application/ServiceExtensions.cs
@@ -4,14 +4,16 @@ using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
 using ShoppingListApp.Application.Common.Behaviors;
 using System.Reflection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 
 namespace ShoppingListApp.Application;
 public static class ServiceExtensions
 {
-    public static void ConfigureApplicationServices(this IServiceCollection services)
+    public static void ConfigureApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddMediatR(_ => _.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-        services.AddAutoMapper(Assembly.GetExecutingAssembly());     
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
     }
